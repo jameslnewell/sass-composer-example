023922f15630f9ca67847a254b5f5b9f183938b4
eastwood <clintryan3@gmail.com>
2016-03-09 14:17:30 +1100

    Initial

---
+++ gulpfile.js
@@ -0,0 +1,46 @@
+var gulp = require('gulp');
+var path = require('path');
+var readdir = require('readdir');
+var sequence = require('run-sequence');
+
+var cfg = {
+  production: process.argv.indexOf('all') !== -1,
+  srcdir: './src',
+  distdir: './dist',
+  reportsdir: './dist/__reports__'
+};
+
+//load gulp tasks
+var taskDir = path.join(__dirname, 'tasks');
+var taskFiles = readdir.readSync(taskDir, ['**.js']);
+taskFiles.forEach(function(taskFile) {
+  require(path.join(taskDir, taskFile))(cfg);
+});
+
+gulp.task('build', function(done) {
+  sequence("scripts.lint", ["scripts.bundle","styles.bundle","content.build"], done);
+});
+
+gulp.task('test', function(done) {
+  sequence("scripts.test", done);
+});
+
+gulp.task('test.watch', function(done) {
+  sequence("scripts.test.watch", done);
+});
+
+gulp.task('optimise', function(done) {
+  sequence(["scripts.optimise","styles.optimise","images.optimise"], "cachebust", done);
+});
+
+gulp.task('watch', function(done) {
+  sequence(["scripts.watch","styles.watch","content.watch"], done);
+});
+
+gulp.task('default', function(done) {
+  sequence("build", done);
+});
+
+gulp.task('all', function(done) {
+  sequence("build", "test", "optimise", done);
+});
